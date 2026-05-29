14ac40397cf1c221f77f8a6332779c70092675b9
Beterraba <ls6182315@gmail.com>
2026-05-28 23:00:48 -0300

    update API URLs to include versioning and add schema documentation endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("items/", views.items, name="items"),
+    path("api/v1/items/", views.items, name="items"),
+    path('api/v1/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path('api/v1/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs'),
 ]
