11c2dc699bc50453b3a3d5964f7f02d9c13aea86
Beterraba <ls6182315@gmail.com>
2026-05-28 22:13:07 -0300

    refactor items view to use a constant for API URL

--- api/views.py
+++ api/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 import requests
 
+API_URL = "https://testedefensoriapr.pythonanywhere.com/precos"
 
 def items(request):
-    response = requests.get("https://testedefensoriapr.pythonanywhere.com/precos")
+    response = requests.get(API_URL)
     return HttpResponse(response.content, content_type="application/json")
